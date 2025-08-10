89440d903ca75781b64f12fa0008b48ef87c2144
safefreightprogram <info@safefreightprogram.com>
2025-08-10 20:23:29 +1000

    Create sfp-config.js

---
+++ js/sfp-config.js
@@ -0,0 +1,3 @@
+// /js/sfp-config.js — central place for GAS endpoints (loaded on every page)
+window.SFP_GAS_BASE = "https://script.google.com/macros/s/AKfycbx12OZCmxXdHJEzqyANM__C74534vzAHg8yEPsJ_nlt1COExwrbEzat_c4-C2gmUVQnkw/exec";
+window.SFP_ROLES_URL = `${window.SFP_GAS_BASE}?action=getRole`;
